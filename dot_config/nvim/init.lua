49f8909f9af1814a567c0f9a6f1af1bb25fa7c04
Gary Cheeseman <gary@five57.com>
2021-11-24 14:35:14 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,16 +5,15 @@ set cpoptions+=$                " Show $ at the end of a change command
 set foldopen+=jump
 set shortmess+=c                " Don't pass messages to ins-completion-menu.
 set nocompatible
-set viminfo='10,\"100,:20,%,n/Users/gcman105/.viminfo
 " set viminfo=%,<800,'10,/50,:100,h,f0,n/Users/gcman105/.viminfo
-"           | |    |   |   |    | |  + viminfo file path
-"           | |    |   |   |    | + file marks 0-9,A-Z 0=NOT stored
-"           | |    |   |   |    + disable 'hlsearch' loading viminfo
-"           | |    |   |   + command-line history saved
-"           | |    |   + search history saved
-"           | |    + files marks saved
-"           | + lines saved each register (old name for <, vi6.2)
-"           + save/restore buffer list
+  "           | |    |   |   |    | |  + viminfo file path
+  "           | |    |   |   |    | + file marks 0-9,A-Z 0=NOT stored
+  "           | |    |   |   |    + disable 'hlsearch' loading viminfo
+  "           | |    |   |   + command-line history saved
+  "           | |    |   + search history saved
+  "           | |    + files marks saved
+  "           | + lines saved each register (old name for <, vi6.2)
+  "           + save/restore buffer list
 hi ColorColumn ctermbg=darkgrey guibg=darkgrey
 ]])
 
