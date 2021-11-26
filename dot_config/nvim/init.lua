3893802baf6d1b49c47f292962ee52aa73158dd2
Gary Cheeseman <gary@five57.com>
2021-11-26 18:09:12 +0100

    Update dot_config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -250,7 +250,7 @@ require('telescope').setup {
   }
 }
 
-require('telescope').load_extension('fzf'),
+require('telescope').load_extension('fzf')
 
 -- Autocommands --------------------------------------------------------------
 vim.cmd([[
