7e7580d5254157bdb16c1f569be9117966df26c7
Gary Cheeseman <gary@five57.com>
2021-12-13 12:53:17 +0100

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -19,16 +19,16 @@ return require('packer').startup(function()
 
     use 'neovim/nvim-lspconfig'
 
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-cmdline"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-    use "hrsh7th/cmp-nvim-lua"
-    use "hrsh7th/cmp-nvim-lsp"
-    use "hrsh7th/cmp-nvim-lsp-document-symbol"
+    -- use "hrsh7th/nvim-cmp"
+    -- use "hrsh7th/cmp-cmdline"
+    -- use "hrsh7th/cmp-buffer"
+    -- use "hrsh7th/cmp-path"
+    -- use "hrsh7th/cmp-nvim-lua"
+    -- use "hrsh7th/cmp-nvim-lsp"
+    -- use "hrsh7th/cmp-nvim-lsp-document-symbol"
 
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
+    -- use 'L3MON4D3/LuaSnip'
+    -- use 'saadparwaiz1/cmp_luasnip'
 
     use {
         'nvim-treesitter/nvim-treesitter',
