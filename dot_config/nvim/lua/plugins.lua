76ca8cf800d9bfd02e408d08c8d4f5c6fe73cc1a
Gary Cheeseman <gary@five57.com>
2021-12-13 12:36:20 +0100

    Update dot_config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -47,12 +47,12 @@ return require('packer').startup(function()
         end
     }
 
-    use 'ThePrimeagen/harpoon'
-    use 'farmergreg/vim-lastplace'
+    -- use 'ThePrimeagen/harpoon'
+    -- use 'farmergreg/vim-lastplace'
     -- use 'jiangmiao/auto-pairs'
     use 'liuchengxu/vim-which-key'
-    -- use 'majutsushi/tagbar'
-    -- use 'mbbill/undotree'
+    use 'majutsushi/tagbar'
+    use 'mbbill/undotree'
     use 'mcchrish/nnn.vim'
     use 'mhinz/vim-startify'
     use 'nvim-lua/plenary.nvim'
