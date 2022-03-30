cd24cccfd3bce5ee7054cfeb6c1cd925f6fb9193
Gary Cheeseman <gary@five57.com>
2022-03-30 10:14:51 +0200

    Update dot_config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -17,6 +17,7 @@ set nocompatible
 hi ColorColumn ctermbg=darkgrey guibg=darkgrey
 filetype plugin on
 syntax on
+:colorscheme nightfox
 ]])
 
 --vim.o.guicursor = n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175
