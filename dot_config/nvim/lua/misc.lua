1ad73e956899f20235c0b3266de71dd137dc2639
Gary Cheeseman <gary@five57.com>
2022-03-30 10:10:23 +0200

    Update dot_config/nvim/lua/misc.lua

--- dot_config/nvim/lua/misc.lua
+++ dot_config/nvim/lua/misc.lua
@@ -1,7 +1,8 @@
 -- Load by calling `lua require('misc')` from your init.vim
 
 -- Theme ---------------------------------------------------------------------
-require('nightfox').load('nightfox')
+:colorscheme nightfox
+-- require('nightfox').load('nightfox')
 -- require('nightfox').load('duskfox')
 
 require'lualine'.setup {
