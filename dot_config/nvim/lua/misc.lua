2206b0069f6befa7b6d32f3ba63305509915d421
Gary Cheeseman <gary@five57.com>
2022-06-07 16:09:22 +0200

    Update dot_config/nvim/lua/misc.lua

--- dot_config/nvim/lua/misc.lua
+++ dot_config/nvim/lua/misc.lua
@@ -176,9 +176,9 @@ require'hop'.setup{}
 
 -- Treesitter ----------------------------------------------------------------
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
     ignore_install = { }, -- List of parsers to ignore installing
+    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
     highlight = {
         enable = true,              -- false will disable the whole extension
         disable = { },  -- list of language that will be disabled
